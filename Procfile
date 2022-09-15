806a37fc3e93e9279944090c816586090331e280
NicoLorDev97 <nicolasemanuellorusso97@gmail.com>
2022-09-15 11:30:52 -0300

    Final commit

--- Procfile
+++
@@ -1,1 +0,0 @@
-worker: uvicorn main.py
--- test.py
+++ test.py
@@ -4,13 +4,3 @@ import pymysql
 import json
 from Configuracion import password
 
-conexion = pymysql.connect(host="localhost",
-                           user = "root",
-                           password=password,
-                           database="proyectoindividual"
-                           )
-
-cursor = conexion.cursor()
-
-cursor.execute("SELECT d.Full_Name, COUNT(vista.DriverId) as Ganadas, c.Nationality as Nacionalidad, vista.DriverId FROM (SELECT r.DriverId, Position, r.ConstructorId FROM results r WHERE Position = 1 ORDER BY r.DriverId) as vista INNER JOIN drivers d ON vista.DriverId = d.DriverId INNER JOIN constructors c ON c.ConstructorId =  vista.ConstructorId WHERE c.Nationality = 'American' OR c.Nationality = 'British' GROUP BY r.DriverId LIMIT 1;")
-
